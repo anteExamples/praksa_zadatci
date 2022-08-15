72beed09d9c2ac838c701af55cbee35badc6c848
Ante Miocic <ante.miocic@alfatec.hr>
2022-08-15 22:57:18 +0200

    md file

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import * as path from 'path'
 import { fileURLToPath } from 'url';
 import router from './routes/products.routes.js';
+import {initiDB} from './db.js'
 
 // Initiating express
 var app = express();
@@ -30,6 +31,9 @@ app.use(router);
   
 // App is run on port 3000 so just run http://localhost:3000/ to start app
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    // Create products table and populate on init
+    await initiDB();
+    
     console.log(`Our app is running on port ${ PORT }`);
 });
--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,10 +1,8 @@
-import {getAllData, initiDB} from '../db.js'
+import {getAllData} from '../db.js'
 
 // Service for getting view with all products displayed
 export const fetchProducts = async (req, res)  => {
     try {
-        await initiDB();
-
         const tableData = await getAllData();
 
         res.render('productsList', { tableData });
